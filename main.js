78b7f9850c130992ed12fb480943d6b09d98f288
arnavkh21 <72095856+arnavkh21@users.noreply.github.com>
2021-08-16 12:21:41 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 song = "";
-
+leftWristX = 0;
+leftWristY= 0;
+rightWristY= 0;
+rightWristX=0;
 function preload(){
     song = loadSound("music.mp3");
 }
 
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
 
 function setup(){
@@ -14,8 +19,28 @@ canvas.center();
 
 video = createCapture(VIDEO);
 video.hide();
+
+poseNet = ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
 }
 
 function draw(){
     image(video,0,0,600,500)
+}
+
+function modelLoaded(){
+    console.log('PoseNet is inintiallizedd');
+}
+
+function gotPoses(results){
+    if(results.length > 0 ){
+        console.log(results);
+        leftWristX = results[0].pose.leftWristX.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = "+ leftWristX + "leftWristY"+ leftWristY);
+       
+        rightWristX = results[0].pose.rightWristX.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("righttWristX = "+ rightWristX + "rightWristY = " + rightWristY);
+    }
 }
